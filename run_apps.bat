3c92e39b5d14448409d934841c8bda6ae08a145c
adwaita shinde <sadwaita2001@gmail.com>
2026-04-06 21:16:29 +0530

    "CleanupBatchFiles"

--- run_apps.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Backend...
-cd backend
-start "Backend" /B npm run start:dev
-echo Starting Frontend...
-cd ../frontend
-start "Frontend" /B npm run dev
-echo Servers started in background.
